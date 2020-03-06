cd3df9007901d57a47c3fdab459083721f187ce4
wmollenkopf <wmollenkopf@bixel.dev>
2020-03-06 16:20:53 -0500

    Basic Initial layout of website.

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
+      <div id='title'>
+        <span id="mainTitle">
+          bixel.dev
+        </span>
+        <hr/>
+        <span id="subTitle">
+          <div>Projects</div>
+          <div>Games</div>
+          <div>Blog</div>
+          <div>About</div>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
