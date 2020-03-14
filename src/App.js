d5bd686d1e27dcead3818de96c6a9851a2b9a68d
wmollenkopf <12564813+wmollenkopf@users.noreply.github.com>
2020-03-13 22:21:50 -0400

    More work on the navigational links. With more to come.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import "./navbar.css";
 import NavList from "./Components/Navbar/NavList"
-import { FaTwitch } from 'react-icons/fa';
 
 export default class App extends Component {
-  PAGE_NAMES = {
-    HOME: "Home",
-    PROJECTS: "Projects",
-    GAMES: "Games",
-    BLOG: "Blog",
-    ABOUT: "About"
-  };
+  // constructor(props) {
+  //   super(props);
 
-  pages = [this.PAGE_NAMES.HOME,this.PAGE_NAMES.PROJECTS,this.PAGE_NAMES.GAMES,this.PAGE_NAMES.BLOG,this.PAGE_NAMES.ABOUT];
-  
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      toggle: false,
-      currentPage: this.PAGE_NAMES.PROJECTS
-    };
-  }
-
-  navToggle = () => {
-    this.setState({ toggle: !this.state.toggle });
-    console.log("changed", this.state.toggle);
-  };
-
-  newRoute = e => {
-    e.preventDefault();
-    //newRoute
-    let anchor = e.target.hash;
-    console.log(anchor);
-    this.setState({ currentPage: anchor });
-  };
+  // }
 
   render() {
-  //   const PageLinks = this.pages.map((page, index) =>
-  //   <li key={index}>
-  //     {page}
-  //   </li>
-  // );
     return (
       <div className="App">
         <div className="starStuff">
@@ -51,36 +16,7 @@ export default class App extends Component {
           <div id="stars2"></div>
           <div id="stars3"></div>
         </div>
-        <NavList pages={this.pages} activePage={this.state.currentPage} newRoute={this.newRoute} />
-
-        <ul id="topnav" className={this.state.toggle ? "" : "hide-nav-links"}>
-        <li
-            id="projects"
-            className={
-              this.currentPage === this.PAGE_NAMES.PROJECTS ? "active " : "active"
-            }
-          >
-            <a href="#projects" onClick={this.newRoute}>
-              Home
-            </a>
-          </li>
-          <li className="navLink">
-            <a href="#games" onClick={this.newRoute}>
-              Games
-            </a>
-          </li>
-          <li className="navLink">
-            <a href="#blog" onClick={this.newRoute}>
-              FFXIV
-            </a>
-          </li>
-          <li className="navLink">
-            <a href="#about" onClick={this.newRoute}>
-              <FaTwitch /> Twitch
-            </a>
-          </li>
-        </ul>
-
+        <NavList newRoute={this.newRoute}  />
         <div id="title">
           <span id="mainTitle">bixel.dev</span>
           <hr />
--- src/components/Navbar/NavList.js
+++ src/components/Navbar/NavList.js
@@ -1,43 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaTwitch } from 'react-icons/fa';
+import "./navbar.css";
 
-const NavList = ({ pages,activePage,newRoute }) => {
-  return (
-    <ul id="topnav">
-      {
-        
-          
-        pages.map((page, i) => {
-          return (
-            <li
-            id="projects"
-            key={i}
-            className={activePage === page
-                ? "active "
-                : "active"
-            }
-          >
 
-          {
-            
-          // <a href={page} onClick={this.newRoute}>
-          // {page}
-          //   </a>
+export default class NavList extends Component {
+  constructor(props) {
+    super(props);
 
-          }
+    this.state = {
+      toggle: false,
+      currentPage: this.PAGE_NAMES.HOME
+    };
+  }
 
-          
-          </li>
-            // <NavLink
-            //   // key={i}
-            //   // id={robots[i].id}
-            //   // name={robots[i].name}
-            //   // email={robots[i].email}
-            //   />
-          );
-        })
-      }
+  PAGE_NAMES = {
+    HOME: "Home",
+    GAMES: "Games",
+    FFXIV: "FFXIV",
+    TWITCH: "Twitch"
+  };
+
+  newRoute = e => {
+    //e.preventDefault();
+    //newRoute 
+    let destination = (e.target.hash).slice(1);
+    console.log(destination);
+    this.setState({ currentPage: destination });
+    if (destination === this.PAGE_NAMES.TWITCH) {
+      window.location = "https://www.twitch.tv/BiriBixel";
+    } else {
+      console.log("Not traveling:", destination)
+    }
+  };
+
+  // const pages = [this.PAGE_NAMES.HOME, this.PAGE_NAMES.PROJECTS, this.PAGE_NAMES.GAMES, this.PAGE_NAMES.BLOG, this.PAGE_NAMES.ABOUT];
+  render() {
+    let newRoute = this.newRoute;
+    return (
+      <ul id="topnav" >
+        <li id="Home" className={this.state.currentPage === this.PAGE_NAMES.HOME ? "active " : this.state.currentPage}>
+          <a href="#Home" onClick={newRoute}>Home</a>
+        </li>
+        <li id="Games" className={this.state.currentPage === this.PAGE_NAMES.GAMES ? "active " : this.state.currentPage}>
+          <a href="#Games" onClick={newRoute}>Games</a>
+        </li>
+        <li id="FFXIV" className={this.state.currentPage === this.PAGE_NAMES.FFXIV ? "active " : this.state.currentPage}>
+          <a href="#FFXIV" onClick={newRoute}>FFXIV</a>
+        </li>
+        <li id="Twitch" className={this.state.currentPage === this.PAGE_NAMES.TWITCH ? "active " : this.state.currentPage}>
+          <a href="#Twitch" onClick={newRoute}><FaTwitch /> Twitch</a>
+        </li>
       </ul>
-  );
+    );
+  }
 }
-
-export default NavList;
