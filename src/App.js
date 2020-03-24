e0ae76298135513f92dc9ec1f09c234081c374e2
wmollenkopf <12564813+wmollenkopf@users.noreply.github.com>
2020-03-24 16:33:30 -0400

    Constants file setup, and prepped App for lazyload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
 import NavList from "./Components/Navbar/NavList"
+import { PAGE_NAMES } from "./Components/Constants.js";
 
 export default class App extends Component {
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
 
-  // }
+    this.state = {
+      toggle: false,
+      currentPage: PAGE_NAMES.HOME
+    };
+
+  }
 
   render() {
+    const newRoute = (e) => {
+      e.preventDefault();
+      //newRoute 
+      let destination = e.target.getAttribute('href').slice(1);
+      console.log(destination);
+      this.setState({ currentPage: destination });
+      if (destination === PAGE_NAMES.TWITCH) {
+        window.location = "https://www.twitch.tv/BiriBixel";
+      } else {
+        console.log("Not traveling:", destination)
+      }
+    };
     return (
       <div className="App">
         <div className="starStuff">
@@ -16,7 +34,7 @@ export default class App extends Component {
           <div id="stars2"></div>
           <div id="stars3"></div>
         </div>
-        <NavList newRoute={this.newRoute}  />
+        <NavList newRoute={newRoute} currentPage={this.state.currentPage}  />
         <div id="title">
           <span id="mainTitle">bixel.dev</span>
           <hr />
--- src/components/Navbar/NavList.js
+++ src/components/Navbar/NavList.js
@@ -1,56 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FaTwitch } from 'react-icons/fa';
 import "./navbar.css";
+import { PAGE_NAMES } from "../Constants.js";
 
-
-export default class NavList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      toggle: false,
-      currentPage: this.PAGE_NAMES.HOME
-    };
-  }
-
-  PAGE_NAMES = {
-    HOME: "Home",
-    GAMES: "Games",
-    FFXIV: "FFXIV",
-    TWITCH: "Twitch"
-  };
-
-  newRoute = e => {
-    //e.preventDefault();
-    //newRoute 
-    let destination = (e.target.hash).slice(1);
-    console.log(destination);
-    this.setState({ currentPage: destination });
-    if (destination === this.PAGE_NAMES.TWITCH) {
-      window.location = "https://www.twitch.tv/BiriBixel";
-    } else {
-      console.log("Not traveling:", destination)
-    }
-  };
-
-  // const pages = [this.PAGE_NAMES.HOME, this.PAGE_NAMES.PROJECTS, this.PAGE_NAMES.GAMES, this.PAGE_NAMES.BLOG, this.PAGE_NAMES.ABOUT];
-  render() {
-    let newRoute = this.newRoute;
+const NavList = ({newRoute, currentPage }) => {
     return (
       <ul id="topnav" >
-        <li id="Home" className={this.state.currentPage === this.PAGE_NAMES.HOME ? "active " : this.state.currentPage}>
+        <li id="Home" className={currentPage === PAGE_NAMES.HOME ? "active " : currentPage}>
           <a href="#Home" onClick={newRoute}>Home</a>
         </li>
-        <li id="Games" className={this.state.currentPage === this.PAGE_NAMES.GAMES ? "active " : this.state.currentPage}>
+        <li id="Games" className={currentPage === PAGE_NAMES.GAMES ? "active " : currentPage}>
           <a href="#Games" onClick={newRoute}>Games</a>
         </li>
-        <li id="FFXIV" className={this.state.currentPage === this.PAGE_NAMES.FFXIV ? "active " : this.state.currentPage}>
+        <li id="FFXIV" className={currentPage === PAGE_NAMES.FFXIV ? "active " : currentPage}>
           <a href="#FFXIV" onClick={newRoute}>FFXIV</a>
         </li>
-        <li id="Twitch" className={this.state.currentPage === this.PAGE_NAMES.TWITCH ? "active " : this.state.currentPage}>
+        <li id="Twitch" className={currentPage === PAGE_NAMES.TWITCH ? "active " : currentPage}>
           <a href="#Twitch" onClick={newRoute}><FaTwitch /> Twitch</a>
         </li>
       </ul>
     );
-  }
 }
+
+export default NavList;
